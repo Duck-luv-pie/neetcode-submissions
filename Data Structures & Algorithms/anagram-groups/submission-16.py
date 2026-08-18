3d3685b774362aa2512710fc70e3cd3f6fab53ea
Eric Yoon <113267079+Duck-luv-pie@users.noreply.github.com>
2026-08-18 12:10:28 -0400

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for string in strs:
+            alpha = [0] * 26
+
+            for char in string:
+                alpha[ord(char) - ord('a')] += 1
+            
+            key = tuple(alpha)
+
+            if key in groups:
+                groups[key].append(string)
+            else:
+                groups[key] = [string]
+        
+        return list(groups.values())
+            
