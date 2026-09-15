a9d7c580dc14628d7d1da11fb150f20404c6608c
Eric Yoon <113267079+Duck-luv-pie@users.noreply.github.com>
2026-09-15 19:06:42 -0400

    Add: meeting-schedule - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule/submission-11.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        if not intervals:
+            return True
+        prev_end = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < prev_end:
+                return False
+            prev_end = interval.end
+        
+        return True
