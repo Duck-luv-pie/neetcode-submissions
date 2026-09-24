7d3de335887b39ff1aff469086c29ccd3d056043
Eric Yoon <113267079+Duck-luv-pie@users.noreply.github.com>
2026-09-23 23:45:17 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+
+        res, path = [], []
+
+        def dfs(start, remaining):
+            if remaining == 0:
+                res.append(path[:])
+                return
+            
+            for j in range(start, len(nums)):
+                c = nums[j]
+
+                if c > remaining:
+                    break
+                path.append(nums[j])
+
+                dfs(j, remaining - c)
+
+                path.pop()
+            
+        dfs(0, target)
+        return res
